e6efef87fe7c390b9bc7e45fe6068f6f9615e3b8
PJ <pjloury@gmail.com>
2024-12-30 17:32:20 -0800

    Initial Commit

---
+++ aerial-landscapes/aerial_landscapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  aerial_landscapesApp.swift
+//  aerial-landscapes
+//
+//  Created by PJ Loury on 12/30/24.
+//
+
+import SwiftUI
+
+@main
+struct aerial_landscapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
